1b8b2dea805bbd289fa5a47fdd3ec1be74ced7a1
Adam Grygar <grygarad@gmail.com>
2020-12-18 17:59:13 +0100

    Added colors constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,6 @@
+export const primaryColor = '#9c27b0';
+export const infoColor = '#00acc1';
+export const successColor = '#4caf50';
+export const dangerColor = '#f44336';
+export const waringColor = '#ff9800';
+export const roseColor = '#e91e63';
