bed4a9186100810702961b7f5249be8d599bdb7d
Adam Grygar <grygarad@gmail.com>
2020-12-27 21:26:02 +0100

    Added logout button to AppBar

--- src/routes/DashboardRoutes.tsx
+++ src/routes/DashboardRoutes.tsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react"
-import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core"
+import { AppBar, Button, IconButton, makeStyles, createStyles, Theme, Toolbar, Typography } from "@material-ui/core"
 import { Menu } from "@material-ui/icons"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Dashboard } from "../components/Dashboard/Dashboard";
 import { UniversalDrawer } from "../components/UniversalDrawer";
 import { DashboardDrawerList } from "../constants/RoutesConstants";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
 export const DashboardRoutes = () => {
+    const classes = useStyles();
     const [open, setOpen] = useState<boolean>(false);
 
     const handleDrawerOpen = () => {
@@ -21,12 +33,13 @@ export const DashboardRoutes = () => {
         <Router>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleDrawerOpen} >
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerOpen} >
                         <Menu />
                     </IconButton>
-                    <Typography variant="h6" >
+                    <Typography variant="h6" className={classes.title}>
                         Dashboard
                     </Typography>
+                    <Button color="inherit" href="/signin">Log out</Button>
                 </Toolbar>
             </AppBar>
             <Switch>
