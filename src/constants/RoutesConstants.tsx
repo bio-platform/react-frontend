6371d5e20240dd2e24139c2fb2b8b6a5205ffe05
Adam Grygar <grygarad@gmail.com>
2020-12-27 20:45:20 +0100

    Changed overview to limits

--- src/constants/RoutesConstants.tsx
+++ src/constants/RoutesConstants.tsx
@@ -3,4 +3,4 @@ import ListIcon from '@material-ui/icons/List';
 import InfoIcon from '@material-ui/icons/Info';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 
-export const DashboardDrawerList = [['Create new instance', <AddCircleIcon />], ['Overview', <InfoIcon />], ['Instances', <ListIcon />]];
+export const DashboardDrawerList = [['Create new instance', <AddCircleIcon />], ['Limits', <InfoIcon />], ['Instances', <ListIcon />]];
