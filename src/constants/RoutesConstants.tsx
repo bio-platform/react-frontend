74581c223616211c9b20aef99687a943bfa5eff2
Adam Grygar <grygarad@gmail.com>
2020-12-27 19:08:27 +0100

    Added clarifying comment

--- src/constants/RoutesConstants.tsx
+++ src/constants/RoutesConstants.tsx
@@ -1,4 +1,5 @@
 import React from "react";
 import { Dashboard } from "@material-ui/icons";
 
+// these are icons
 export const DashboardDrawerList = [['Dashboard', <Dashboard />], ['Instances', <Dashboard />], ['Volumes', <Dashboard />]];
