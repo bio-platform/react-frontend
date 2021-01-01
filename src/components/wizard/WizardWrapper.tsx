69f6ea1e9f83d930f2e23fc89308a4b99c5d4383
Adam Grygar <grygarad@gmail.com>
2021-01-01 17:05:11 +0100

    Added base wizard wrapper code

--- src/components/wizard/WizardWrapper.tsx
+++ src/components/wizard/WizardWrapper.tsx
@@ -1,4 +1,4 @@
-import { Stepper, Step, StepLabel } from '@material-ui/core';
+import { Stepper, Step, StepLabel, Container, Button } from '@material-ui/core';
 import React, { useState } from 'react';
 
 export const WizardWrapper = () => {
@@ -6,13 +6,17 @@ export const WizardWrapper = () => {
 
 
     return (
-        <Stepper activeStep={activeStep}>
-            <Step key={'1'}>
-                <StepLabel>step 1</StepLabel>
-            </Step>
-            <Step key={'2'}>
-                <StepLabel>step 2</StepLabel>
-            </Step>
-        </Stepper>
+        <Container maxWidth='xl'>
+            {activeStep === 0 ? 'first' : 'second'}
+            <Button onClick={() => setActiveStep(activeStep + 1)}>Next</Button>
+            <Stepper activeStep={activeStep} >
+                <Step key={'1'}>
+                    <StepLabel>step 1</StepLabel>
+                </Step>
+                <Step key={'2'}>
+                    <StepLabel>step 2</StepLabel>
+                </Step>
+            </Stepper>
+        </Container>
     )
 }
