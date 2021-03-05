42f2f27303ecfe427d809989e82c9bdb9890b0b0
Adam Grygar <grygarad@gmail.com>
2021-03-05 09:45:13 +0100

    Added border margin

--- src/components/Dashboard/InstancesTable.tsx
+++ src/components/Dashboard/InstancesTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, makeStyles, Typography } from '@material-ui/core';
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, makeStyles, Typography, Box } from '@material-ui/core';
 import { Instance } from '../../models/Instance';
 import { DeleteInstanceButton } from '../static/CustomButtons';
 
@@ -27,33 +27,35 @@ export const InstancesTable = ({ instances, reloadData }: Props) => {
     }
 
     return (
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="instances table">
-                <TableHead>
-                    <TableRow className={classes.tableRow}>
-                        <TableCell>Instance name</TableCell>
-                        <TableCell align="left">Floating IP</TableCell>
-                        <TableCell align="left">Cores</TableCell>
-                        <TableCell align="left">RAM</TableCell>
-                        <TableCell align="center">Action</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {instances.map((instance) => (
-                        <TableRow className={classes.tableRow} key={instance.id}>
-                            <TableCell component="th" scope="row">
-                                {instance.name}
-                            </TableCell>
-                            <TableCell align="left">{instance.status}</TableCell>
-                            <TableCell align="left">{instance.status}</TableCell>
-                            <TableCell align="left">{instance.status} GB</TableCell>
-                            <TableCell align="center">
-                                <DeleteInstanceButton instance={instance} reloadData={reloadData} />
-                            </TableCell>
+        <Box mb={6}>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="instances table">
+                    <TableHead>
+                        <TableRow className={classes.tableRow}>
+                            <TableCell>Instance name</TableCell>
+                            <TableCell align="left">Floating IP</TableCell>
+                            <TableCell align="left">Cores</TableCell>
+                            <TableCell align="left">RAM</TableCell>
+                            <TableCell align="center">Action</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {instances.map((instance) => (
+                            <TableRow className={classes.tableRow} key={instance.id}>
+                                <TableCell component="th" scope="row">
+                                    {instance.name}
+                                </TableCell>
+                                <TableCell align="left">{instance.status}</TableCell>
+                                <TableCell align="left">{instance.status}</TableCell>
+                                <TableCell align="left">{instance.status} GB</TableCell>
+                                <TableCell align="center">
+                                    <DeleteInstanceButton instance={instance} reloadData={reloadData} />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 }
