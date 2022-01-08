fabbaefb56cb512837235349098bb9e750edeefb
Adam Grygar <grygarad@gmail.com>
2022-01-08 20:08:22 +0100

    Extract domain name

--- src/constants/Environment.ts
+++ src/constants/Environment.ts
@@ -1,6 +1,7 @@
 export const OIDC_AUTHORITY = process.env.REACT_APP_AUTHORITY;
 export const OIDC_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const OIDC_REDIRECT_URI = 'http://localhost:4200/callback/';
-export const OIDC_POST_LOGOUT_REDIRECT_URI = 'http://localhost:4200/auth/';
+const domain = 'http://localhost:4200/';
+export const OIDC_REDIRECT_URI = `${domain}callback/`;
+export const OIDC_POST_LOGOUT_REDIRECT_URI = `${domain}auth/`;
 
 export const API_URL = 'https://ip-147-251-124-112.flt.cloud.muni.cz/api/';
