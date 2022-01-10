d448572f65ff92c593a4b91c7a250a5761f6f6dc
Adam Grygar <grygarad@gmail.com>
2022-01-10 21:22:54 +0100

    Added new API adress

--- src/constants/Environment.ts
+++ src/constants/Environment.ts
@@ -1,7 +1,8 @@
 export const OIDC_AUTHORITY = process.env.REACT_APP_AUTHORITY;
 export const OIDC_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const domain = 'http://localhost:4200/';
+// const domain = 'https://bio-portal.metacentrum.cz/';
 export const OIDC_REDIRECT_URI = `${domain}callback/`;
 export const OIDC_POST_LOGOUT_REDIRECT_URI = `${domain}auth/`;
 
-export const API_URL = 'https://ip-147-251-124-112.flt.cloud.muni.cz/api/';
+export const API_URL = 'https://bio-portal.metacentrum.cz/api/';
