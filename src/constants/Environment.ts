88589020f6d8362682aec6bbc103bc1b8ebb7db4
Adam Grygar <grygarad@gmail.com>
2022-03-06 11:57:11 +0100

    Added bias for environment vars

--- src/constants/Environment.ts
+++ src/constants/Environment.ts
@@ -1,8 +1,22 @@
 export const OIDC_AUTHORITY = process.env.REACT_APP_AUTHORITY;
 export const OIDC_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-const domain = 'http://localhost:4200/';
-// const domain = 'https://bio-portal.metacentrum.cz/';
+
+let domain = 'http://localhost:4200/';
+let api = 'https://bio-portal.metacentrum.cz/';
+if (window.location.hostname === 'localhost') {
+	domain = 'http://localhost:4200/';
+	api = 'https://bio-portal.metacentrum.cz/';
+} else if (window.location.hostname === 'bio-portal.metacentrum.cz') {
+	domain = 'https://bio-portal.metacentrum.cz/';
+	api = 'https://bio-portal.metacentrum.cz/';
+} else if (
+	window.location.hostname === 'ip-147-251-124-112.flt.cloud.muni.cz'
+) {
+	domain = 'https://ip-147-251-124-112.flt.cloud.muni.cz/';
+	api = 'https://ip-147-251-124-112.flt.cloud.muni.cz/';
+}
+
 export const OIDC_REDIRECT_URI = `${domain}callback/`;
 export const OIDC_POST_LOGOUT_REDIRECT_URI = `${domain}auth/`;
 
-export const API_URL = 'https://bio-portal.metacentrum.cz/api/';
+export const API_URL = `${api}api/`;
